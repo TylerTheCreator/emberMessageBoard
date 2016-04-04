f95759da011ca0bdfa22475941650e1cc561fbe5
tylerthecreator <tylerfinleyy@gmail.com>
2016-04-03 20:08:26 -0700

    add new post

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    rentalFormShow() {
+      this.set('addNewPost', true);
+    },
+
+    save1() {
+      var params = {
+        user: this.get('user'),
+        description: this.get('description'),
+        answers: this.get('answers'),
+      };
+      this.set('addNewPost', false);
+      this.sendAction('save2', params);
+    }
+  }
 });
