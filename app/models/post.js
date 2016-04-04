5eb165ae76fbac5f141c19f1a4e37c166117a30a
tylerthecreator <tylerfinleyy@gmail.com>
2016-04-03 19:46:23 -0700

    add post tile and json data

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    user: DS.attr(),
+    description: DS.attr(),
+    answers: DS.hasMany('answer', { async: true})
+
 });
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-message-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://messageboarddddddyo.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
